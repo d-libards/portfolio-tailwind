3fe56902d17b6f04c1ef00c19e636905e7dc937e
d-libards <dominiquelibardo09@gmail.com>
2024-11-01 21:47:01 +0800

    projects done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,17 @@
+import ProjectsCard from './ProjectsCard';
+import { projects } from '../data';
+import SectionTitle from './SectionTitle';
+
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <section className="py-20 align-element" id="projects">
+      <SectionTitle text="web creations" />
+      <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
+        {projects.map((project) => {
+          return <ProjectsCard key={project.id} {...project} />;
+        })}
+      </div>
+    </section>
+  );
 }
 export default Projects;
